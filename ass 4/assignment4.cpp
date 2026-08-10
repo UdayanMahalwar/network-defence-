5c4a4617933b07c9c1ec24927b2b6dfa73c511f1
Udayan Mahalwar <udayanmahalwar@Udayans-MacBook-Air.local>
2026-08-10 16:39:47 +0530

    final

--- ass 4/assignment4.cpp
+++ ass 4/assignment4.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<map>
 using namespace std;
 class comp{
     public:
-    bool operator()(pair<char,int>&a , pair<char,int>&b)
+    bool operator()(const pair<char,int>&a , const pair<char,int>&b)
     {
         return a.first < b.first;
     }
@@ -18,36 +19,46 @@ int main(){
     getline(cin , PlainText);
     string FinalPlain= "";
     unordered_map<char,bool>visited_in_Plain;
-    for(auto it : PlainText)
+    for(int it = 0 ; it < PlainText.size() ; it++)
     {
-        if(it == ' ')
+        // if(it == ' ')
+        // {
+        //     continue;
+        // }
+        // else
+        auto key = PlainText[it];
+        if((key>='a' && key<='z')||(key>='A' && key<='Z')||(key>='0' && key<='9'))
         {
-            continue;
-        }
-        else
-        {
-            FinalPlain += it;
-            visited_in_Plain[it] = true;
+            FinalPlain += key;
+            visited_in_Plain[key] = true;
         }
     }
     string FinalWord= "";
     for(auto it : word)
     {
-        if(it == ' ')
-        {
-            continue;
-        }
-        else
+        // if(it == ' ')
+        // {
+        //     continue;
+        // }
+        if((it>='a' && it<='z')||(it>='A' && it<='Z')||(it>='0' && it<='9'))
         {
             FinalWord += it;
         }
     }
     cout<<" finalword : "<<FinalWord<<endl;
+    if (FinalWord.empty())
+        {
+            cout << "Invalid word!" << endl;
+            return 0;
+        }
     int left_words =min((((int)FinalPlain.size())%((int)FinalWord.size())) , (int)FinalWord.size() - (((int)FinalPlain.size())%((int)FinalWord.size())));
     cout<<left_words<<endl;
-    for(int i = 26 - left_words ; i<26; i++)
+    for(int i = 0 ; i<26; i++)
     {
+        if(left_words == 0) break;
+        if(visited_in_Plain['a'+i]) continue;
         FinalPlain += ('a' + i);
+        left_words -= 1;
     }
     cout<<" finalPlain : "<<FinalPlain<<endl;
     vector<pair<char,int>>store;
